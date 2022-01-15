dcef129952e80e18874ab40b7e513fd25fe701df
Michiharu Ono <michiharuono77@gmail.com>
2022-01-15 19:59:33 +0900

    route fixed, props passed with element

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Dashboard from './Dashboard';
 import Home from './Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    }
+  }
+
+  render () {
     return (
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={< Home loggedInStatus={this.state.loggedInStatus} />} ></Route>
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     );
-}
+  }
 
-export default App;
+
+}
--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Registration from './auth/Registration';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <Registration />
-    </div>
-  );
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <h1>Stauts: {this.props.loggedInStatus}</h1>
+        <Registration />
+      </div>
+    );
+  }
 }
-
-export default Home;
