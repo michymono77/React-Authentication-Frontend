f8c8caa9cda35a0bac73c219daacf2ea19e2afff
Michiharu Ono <michiharuono77@gmail.com>
2022-01-15 19:31:39 +0900

    Cor error fixed by modifying the backend

--- app/javascript/components/auth/Registration.js
+++ app/javascript/components/auth/Registration.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Registration extends Component {
   constructor (props) {
@@ -15,12 +16,33 @@ export default class Registration extends Component {
   }
 
   handleSubmit(event) {
+    const {
+      email,
+      password,
+      password_confirmation
+    } = this.state;
+
+    axios.post('http://localhost:3001/registrations', {
+      user: {
+        email: email,
+        password: password,
+        password_confirmation: password_confirmation
+      }
+    },
+    { withCredentials: true }// IMPORTANT; this will set the cookie on the client
+    ).then(response => {
+      console.log("Registration response", response)
+    }).catch(error => {
+      console.log("Registration error", error);
+    })
     console.log("form submitted");
     event.preventDefault();
   }
 
   handleChange(event) {
-    console.log('handle change', event)
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
   render () {
@@ -28,7 +50,7 @@ export default class Registration extends Component {
       <form onSubmit={this.handleSubmit}>
         <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
         <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
-        <input type="password_confirmation" name="password_confirmation" placeholder="Password Confirmation" value={this.state.password_confirmation} onChange={this.handleChange} required />
+        <input type="password" name="password_confirmation" placeholder="Password Confirmation" value={this.state.password_confirmation} onChange={this.handleChange} required />
         <button type="submit">Register</button>
 
       </form>
