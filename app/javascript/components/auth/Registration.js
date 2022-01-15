a1c04a7cb62e1084173371496d1134d5c089594a
Michiharu Ono <michiharuono77@gmail.com>
2022-01-15 18:25:08 +0900

    form framework added

--- app/javascript/components/auth/Registration.js
+++ app/javascript/components/auth/Registration.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Registration = () => {
-  return (
-    <div>
-      Registration goes here
-    </div>
-   );
+export default class Registration extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      password_confirmation: "",
+      registrationErrors: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log("form submitted");
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    console.log('handle change', event)
+  }
+
+  render () {
+    return (<div>
+      <form onSubmit={this.handleSubmit}>
+        <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
+        <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
+        <input type="password_confirmation" name="password_confirmation" placeholder="Password Confirmation" value={this.state.password_confirmation} onChange={this.handleChange} required />
+        <button type="submit">Register</button>
+
+      </form>
+    </div>)
+  }
 }
-
-export default Registration;
