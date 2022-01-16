c7807b5f7f09dec573786666313725c133ddf83f
Michiharu Ono <michiharuono77@gmail.com>
2022-01-16 10:57:59 +0900

    login file added, post request tested to work

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import Registration from './auth/Registration';
 import { useNavigate } from 'react-router-dom'
+import Login from './auth/Login';
 
 const Home = (props) => {
 
@@ -23,6 +24,7 @@ const Home = (props) => {
         <h1>Home</h1>
         <h1>Stauts: {props.loggedInStatus}</h1>
         <Registration handleSuccessfulAuth={handleSuccessfulAuth}/>
+        <Login />
       </div>
     );
 }
